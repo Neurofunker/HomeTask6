c21940c221d76897c18ce719a0630d49f80c86e2
Makc <m_stefanenkov@mail.ru>
2023-02-19 15:57:09 +0300

    Решил задачу 41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размер массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Number[{i}] = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int[] arr = CreateArray(M);
+Console.WriteLine(arr);
+int SearchPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+int searchPositive = SearchPositive(arr);
+Console.WriteLine($" Чисел больше = {searchPositive}");
